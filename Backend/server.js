d9c21a3fa9db76995bb6661d7a49238bdaa71be6
123-code <naranjojose256@gmail.com>
2022-04-20 12:36:03 -0500

    Added Backend Routing

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const API_KEY = require(process.env.API_KEY);
-const config = require('../config');
-const port = 5000;
+
+
+//const config = require('Crypto-Wallet/Backend/config/index');
+const port =5000;
 
 async function connect(){
-    return mongoose.connect(config.mongodb.url,{
+    return mongoose.connect('mongodb://localhost:37017/price',{
         useNewUrlParser:true,
     useUnifiedTopology:true,
     });
@@ -14,9 +15,32 @@ async function connect(){
 
 connect().then(()=>{
     console.info("Connected to database successfully");
-    app.listen(port,()=>{
-        console.info(`Listening on port${port}`);
-    });
+   
 }).catch((err)=>{
+    console.info("Error!");
     console.error(err);
+    
 })
+
+
+app.get("/",(req,res)=>{
+    console.info("route Working");
+});
+
+app.get("/billetera",(req,res)=>{
+    console.info("route Working");
+});
+
+app.get("/registro",(req,res)=>{
+console.info("Route working");
+});
+
+app.get("/portal",(req,res)=>{
+    console.info("route working");
+});
+
+
+
+app.listen(port,()=>{
+    console.info(`Listening on port${port}`);
+});

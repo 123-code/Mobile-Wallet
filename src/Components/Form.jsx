23b1855d9f1b69c150f7b41421141354a6e5b147
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-07-11 14:13:54 +0000

    Added Form Frontend Component

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Form = (props)=>{
+    let[Nombre,setNombre] = useState('');
+    let [Cedula,setCedula] = useState(0);
+    let [PIN,setPIN] = useState(0);
+    // stores pin confirmation values 
+    let[conf,setconf] = useState(0);
     return(
         <>
-        <form>
-        <h2>{props}</h2>
-        <input type="text"/>
-        </form>
+  <div>
+    <h1> Nombre </h1>
+    <input type="text" onChange={setNombre=(evt)=>Nombre=evt.target.value}/>
+
+
+
+    <h1> Cédula </h1>
+    <input type="text" onChange={setCedula=(evt)=>Cedula=evt.target.value}/>
+
+    <h1> PIN </h1>
+    <input type="text" onChange={setPIN=(evt)=>PIN=evt.target.value}/>
+
+    <h1> Confirma tu PIN </h1>
+    <input type="text" onChange={setconf=(evt)=>conf=evt.target.value}/>
+
+  </div>
+
         
 
         </>

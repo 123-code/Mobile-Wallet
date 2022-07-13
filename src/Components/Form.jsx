a9f533132a8e16cd97876485a41b72c0efc75c60
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-07-13 16:52:57 +0000

    Updated Form Component

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -6,12 +6,34 @@ const Form = (props)=>{
     let [PIN,setPIN] = useState(0);
     // stores pin confirmation values 
     let[conf,setconf] = useState(0);
+
+const handlesubmit = ()=>{
+  let databody = {
+    "Nombre":Nombre,
+    "Cedula":Cedula,
+    "PIN":PIN,
+    "conf":conf
+  }
+
+  return(
+    fetch('mongodb://localhost:27017/payz',{
+      method:'POST',
+      body: JSON.stringify(databody),
+      headers:{
+        
+      }
+    })
+  )
+ 
+
+}
+
+
     return(
         <>
   <div>
     <h1> Nombre </h1>
     <input type="text" onChange={setNombre=(evt)=>Nombre=evt.target.value}/>
-
 
 
     <h1> Cédula </h1>
@@ -22,6 +44,8 @@ const Form = (props)=>{
 
     <h1> Confirma tu PIN </h1>
     <input type="text" onChange={setconf=(evt)=>conf=evt.target.value}/>
+
+    <button> Guardar </button>
 
   </div>
 
